4f7ce78dcc115cb6e10136a9de05cd8bc8556608
gganeles <gabriel.ganeles@gmail.com>
2024-03-21 16:34:51 +0200

    email debug actions

---
+++ src/routes/contactus/+page.server.js
@@ -0,0 +1,50 @@
+import { fail } from "@sveltejs/kit"
+import { fromEmailAddress, fromEmailName } from "$lib/constants";
+
+/** @type {import('./$types').Actions} */
+export const actions = {
+    default: async ({ request }) => {
+        const data = await request.formData();
+        const email = data.get("email")
+        const name = data.get("name")
+        const subj = data.get("subject")
+        const content = data.get("content")
+
+        if (!email | !name | !subj | !content) {
+            return fail(400, { missing: true })
+        }
+
+        const email_request = new Request("https://api.mailchannels.net/tx/v1/send", {
+            method: "POST",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify({
+                personalizations: [
+                    {
+                        to: [{ email: "gabriel.ganeles@gmail.com" }]
+                    }
+                ],
+                from: {
+                    email: fromEmailAddress,
+                    name: fromEmailName
+                },
+                subject: name + ": " + subj,
+                content: [
+                    {
+                        type: "text/plain",
+                        value: content + "\n" + email
+                    }
+                ]
+            })
+        });
+
+        const response = await fetch(email_request);
+        if (response.status < 400) {
+            return { success: true }
+        } else {
+            const error = await response.text()
+            return { failed: true, errorText: error}
+        }
+    }
+}; 
